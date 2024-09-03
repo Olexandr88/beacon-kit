360e554ec88572db371078e060ea3fe3b09bb818
aBear <abear@berachain.com>
2024-09-03 14:05:09 +0200

    added build tags for xxx consensus binary

--- beacond/cmd/main_comet.go
+++ beacond/cmd/main_comet.go
@@ -1,3 +1,5 @@
+//go:build cometBFT
+
 // SPDX-License-Identifier: BUSL-1.1
 //
 // Copyright (C) 2024, Berachain Foundation. All rights reserved.
---
+++ beacond/cmd/main_xxx.go
@@ -0,0 +1,9 @@
+//go:build xxx
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Can I run my node with a non-Tendermint consensus please?")
+}
